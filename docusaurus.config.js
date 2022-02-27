55a6edf09c1b20d9e9b9075a10af45a7d8451d62
Stephen Fraser <stephen.fraser@digirati.com>
2022-02-27 17:39:21 +0000

    Merge + pin again

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ module.exports = {
   ],
   scripts: [
     // 'https://cdn.jsdelivr.net/npm/@digirati/canvas-panel-web-components@latest',
-    'https://cdn.jsdelivr.net/npm/@digirati/canvas-panel-web-components@1.0.42',
+    'https://cdn.jsdelivr.net/npm/@digirati/canvas-panel-web-components@1.0.43',
     // 'https://cdn.jsdelivr.net/npm/@iiif/vault-helpers@latest/dist/index.umd.js'
   ],
   // clientModules: [
