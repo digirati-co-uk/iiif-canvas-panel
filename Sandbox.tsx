22951838c37770f207b64fd2233dd189f5e4e1ab
Stephen Fraser <stephen.fraser@digirati.com>
2022-03-07 23:32:26 +0000

    Updated styling on sandbox and support for label

--- Sandbox.tsx
+++ Sandbox.tsx
@@ -11,111 +11,126 @@ import {
 } from "@codesandbox/sandpack-react";
 import useThemeContext from '@theme/hooks/useThemeContext';
 
-export function Sandbox(_props: { stacked?: boolean; project: SandpackProps } & SandpackProps) {
-  const { isDarkTheme } = useThemeContext();
-  const { project, ...props } = _props;
+export function Sandbox(_props: { stacked?: boolean; label?: string; project: SandpackProps } & SandpackProps) {
+  const {isDarkTheme} = useThemeContext();
+  const {project, label, ...props} = _props;
 
-  const options = { editorHeight: 540, ...project.options, ...props.options };
+  const options = {editorHeight: 542, ...project.options, ...props.options};
 
-  return <CustomSandpack theme={isDarkTheme ? 'dark' : {
-    ...githubLightTheme,
-    palette: {
-      ...githubLightTheme.palette,
-      defaultBackground: 'rgb(246, 248, 250)'
-    },
-    typography: {
-      monoFont: 'SFMono-Regular, Menlo, Monaco, Consolas, \'Liberation Mono\', \'Courier New\', monospace',
-    },
-  }}  {...project} {...props} options={options} />
+  return <div style={{
+    marginBottom: 40,
+    border: _props.stacked ? '1px solid rgb(229, 231, 235)' : 'none',
+    borderRadius: "5px",
+    fontWeight: 600,
+    color: 'rgb(57, 58, 52)',
+    overflow: 'hidden',
+  }}>
+    {label ?
+    <div style={{
+      background: '#F7F8FA',
+      fontSize: '0.875em',
+      padding: '.6em 1em'
+    }}>{label}</div> : null}
+    <CustomSandpack theme={isDarkTheme ? 'dark' : {
+      ...githubLightTheme,
+      palette: {
+        ...githubLightTheme.palette,
+        defaultBackground: 'rgb(246, 248, 250)'
+      },
+      typography: {
+        monoFont: 'SFMono-Regular, Menlo, Monaco, Consolas, \'Liberation Mono\', \'Courier New\', monospace',
+      },
+    }}  {...project} {...props} options={options}/>
+  </div>
 }
 
 
 export function CustomSandpack(props: SandpackProps & { stacked?: boolean }) {
-    // Combine files with customSetup to create the user input structure
-    const userInputSetup = props.files
-      ? {
-        ...props.customSetup,
-        files: {
-          ...props.customSetup?.files,
-          ...props.files,
-        },
-      }
-      : props.customSetup;
+  // Combine files with customSetup to create the user input structure
+  const userInputSetup = props.files
+    ? {
+      ...props.customSetup,
+      files: {
+        ...props.customSetup?.files,
+        ...props.files,
+      },
+    }
+    : props.customSetup;
 
-    const previewOptions: PreviewProps = {
-      showNavigator: props.options?.showNavigator,
-    };
+  const previewOptions: PreviewProps = {
+    showNavigator: props.options?.showNavigator,
+  };
 
-    const codeEditorOptions: CodeEditorProps = {
-      showTabs: props.options?.showTabs,
-      showLineNumbers: props.options?.showLineNumbers,
-      showInlineErrors: props.options?.showInlineErrors,
-      wrapContent: props.options?.wrapContent,
-      closableTabs: props.options?.closableTabs,
-      initMode: props.options?.initMode,
-      extensions: props.options?.codeEditor?.extensions,
-      extensionsKeymap: props.options?.codeEditor?.extensionsKeymap,
-      readOnly: props.options?.readOnly,
-      showReadOnly: props.options?.showReadOnly,
-    };
+  const codeEditorOptions: CodeEditorProps = {
+    showTabs: props.options?.showTabs,
+    showLineNumbers: props.options?.showLineNumbers,
+    showInlineErrors: props.options?.showInlineErrors,
+    wrapContent: props.options?.wrapContent,
+    closableTabs: props.options?.closableTabs,
+    initMode: props.options?.initMode,
+    extensions: props.options?.codeEditor?.extensions,
+    extensionsKeymap: props.options?.codeEditor?.extensionsKeymap,
+    readOnly: props.options?.readOnly,
+    showReadOnly: props.options?.showReadOnly,
+  };
 
-    const providerOptions: SandpackProviderProps = {
-      openPaths: props.options?.openPaths,
-      activePath: props.options?.activePath,
-      recompileMode: props.options?.recompileMode,
-      recompileDelay: props.options?.recompileDelay,
-      autorun: props.options?.autorun ?? true,
-      bundlerURL: props.options?.bundlerURL,
-      startRoute: props.options?.startRoute,
-      skipEval: props.options?.skipEval,
-      fileResolver: props.options?.fileResolver,
-      initMode: props.options?.initMode,
-      initModeObserverOptions: props.options?.initModeObserverOptions,
-      externalResources: props.options?.externalResources,
-    };
+  const providerOptions: SandpackProviderProps = {
+    openPaths: props.options?.openPaths,
+    activePath: props.options?.activePath,
+    recompileMode: props.options?.recompileMode,
+    recompileDelay: props.options?.recompileDelay,
+    autorun: props.options?.autorun ?? true,
+    bundlerURL: props.options?.bundlerURL,
+    startRoute: props.options?.startRoute,
+    skipEval: props.options?.skipEval,
+    fileResolver: props.options?.fileResolver,
+    initMode: props.options?.initMode,
+    initModeObserverOptions: props.options?.initModeObserverOptions,
+    externalResources: props.options?.externalResources,
+  };
 
-    // Parts are set as `flex` values, so they set the flex shrink/grow
-    // Cannot use width percentages as it doesn't work with
-    // the automatic layout break when the component is under 700px
-    const editorPart = props.options?.editorWidthPercentage || 50;
-    const previewPart = 100 - editorPart;
-    const editorHeight = props.options?.editorHeight;
+  // Parts are set as `flex` values, so they set the flex shrink/grow
+  // Cannot use width percentages as it doesn't work with
+  // the automatic layout break when the component is under 700px
+  const editorPart = props.options?.editorWidthPercentage || 50;
+  const previewPart = 100 - editorPart;
+  const editorHeight = props.options?.editorHeight;
 
-    const Layout = props.stacked ? SandpackStack : SandpackLayout;
+  const Layout = props.stacked ? SandpackStack : SandpackLayout;
 
-    return (
-      <SandpackProvider
-        customSetup={userInputSetup}
-        template={props.template}
-        {...providerOptions}
-      >
-        <ClasserProvider classes={props.options?.classes}>
-          <SandpackThemeProvider theme={props.theme}>
-            <Layout>
-              <SandpackCodeEditor
-                {...codeEditorOptions}
-                customStyle={{
-                  height: props.stacked ? undefined : editorHeight,
-                  flexGrow: editorPart,
-                  flexShrink: editorPart,
-                  minWidth: 700 * (editorPart / (previewPart + editorPart)),
-                }}
-              />
-              <SandpackPreview
-                {...previewOptions}
-                customStyle={{
-                  height: editorHeight,
-                  flexGrow: previewPart,
-                  flexShrink: previewPart,
-                  width: props.stacked ? '100%' : undefined,
-                  minWidth: 700 * (previewPart / (previewPart + editorPart)),
-                }}
-              />
-            </Layout>
-          </SandpackThemeProvider>
-        </ClasserProvider>
-      </SandpackProvider>
-    );
+  return (
+    <SandpackProvider
+      customSetup={userInputSetup}
+      template={props.template}
+      {...providerOptions}
+    >
+      <ClasserProvider classes={props.options?.classes}>
+        <SandpackThemeProvider theme={props.theme}>
+          <Layout>
+            <SandpackCodeEditor
+              {...codeEditorOptions}
+              customStyle={{
+                height: props.stacked ? undefined : editorHeight,
+                flexGrow: editorPart,
+                flexShrink: editorPart,
+                minWidth: 700 * (editorPart / (previewPart + editorPart)),
+              }}
+            />
+            <SandpackPreview
+              {...previewOptions}
+              customStyle={{
+                height: editorHeight,
+                flexGrow: previewPart,
+                flexShrink: previewPart,
+                width: props.stacked ? '100%' : undefined,
+                minWidth: 700 * (previewPart / (previewPart + editorPart)),
+              }}
+            />
+          </Layout>
+        </SandpackThemeProvider>
+      </ClasserProvider>
+    </SandpackProvider>
+  );
 
 }
 
