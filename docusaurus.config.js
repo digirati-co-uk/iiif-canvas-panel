890eb0947ebe7f6af95462ea7d93ba175089aa61
Stephen Fraser <stephen.fraser@digirati.com>
2022-07-14 18:02:32 +0100

    Pinned new version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ module.exports = {
   ],
   scripts: [
     // 'https://cdn.jsdelivr.net/npm/@digirati/canvas-panel-web-components@latest',
-    "https://cdn.jsdelivr.net/npm/@digirati/canvas-panel-web-components@1.0.51",
+    "https://cdn.jsdelivr.net/npm/@digirati/canvas-panel-web-components@1.0.52",
     // 'https://cdn.jsdelivr.net/npm/@iiif/vault-helpers@latest/dist/index.umd.js'
   ],
   // clientModules: [
