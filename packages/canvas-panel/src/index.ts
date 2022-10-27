06ab8a2427731647d2b376cd9cca9efe6ce6e3f4
Stephen Fraser <stephen.fraser@digirati.com>
2022-10-27 13:37:30 +0100

    Metadata panel

--- packages/canvas-panel/src/index.ts
+++ packages/canvas-panel/src/index.ts
@@ -2,5 +2,6 @@ import './web-components/layout-container';
 import './web-components/canvas-panel';
 import './web-components/image-service';
 import './web-components/sequence-panel';
+import './web-components/metadata-panel';
 
 export * from './helpers/annotation-display';
