c85773f2e5e99762b7e52b679edc504105c7ce85
Adam Brin <abrin@getty.edu>
2024-04-15 16:10:39 -0700

    restructuring story for sanity

--- packages/storybook/src/stories/canvas-panel.stories.tsx
+++ packages/storybook/src/stories/canvas-panel.stories.tsx
@@ -34,82 +34,30 @@ export const CanvasWithNavigator = () => {
 
 export const CanvasWithSmallZoom = () => {
 
-  const [canZoomIn, setCanZoomIn] = useState(false);
-  const [canZoomOut, setCanZoomOut] = useState(false);
-  let panel;
-  useEffect(() => {
-    panel = document.querySelector(selector);
-    
-    panel.addEventListener("world-ready", (e) => {
-      // set the initial state based on the image that's loaded into the canvas
-      const detail = (e as any).detail;
-      setCanZoomIn(detail.canZoomIn);
-      setCanZoomOut(detail.canZoomOut);
-    })
-
-
-    panel.addEventListener("zoom", (e) => {
-      const detail = (e as any).detail;
-      setCanZoomIn(detail.canZoomIn);
-      setCanZoomOut(detail.canZoomOut);
-    });
-    allEvents.forEach(type => {
-      panel.addEventListener(type, (e) => { action(type)(e) });
-    })
-
-  }, [document.querySelector(selector) !== undefined]);
-
-
-  return <>
-    <button disabled={!canZoomIn} onClick={() => (document?.querySelector(selector) as any).zoomIn()}>Zoom In</button> 
-    <button disabled={!canZoomOut} onClick={() => (document?.querySelector(selector) as any).zoomOut()}>Zoom Out</button>
-    {/* @ts-ignore */}
-    <canvas-panel manifest-id={welcome} canvas-id='https://media.getty.edu/iiif/manifest/canvas/eaa531a5-e6ea-46a2-b6cd-a161d726f87b.json' />
-  </>
+  return ImageViewer(
+    {
+      manifestUrl: saintGines,
+      canvasId: 'https://media.getty.edu/iiif/manifest/canvas/eaa531a5-e6ea-46a2-b6cd-a161d726f87b.json'
+    }
+  )
 }
 
 export const CanvasWithLandscapeZoom = () => {
 
-  const [canZoomIn, setCanZoomIn] = useState(false);
-  const [canZoomOut, setCanZoomOut] = useState(false);
-  let panel;
-  useEffect(() => {
-    panel = document.querySelector(selector);
-    
-    panel.addEventListener("world-ready", (e) => {
-      // set the initial state based on the image that's loaded into the canvas
-      const detail = (e as any).detail;
-      setCanZoomIn(detail.canZoomIn);
-      setCanZoomOut(detail.canZoomOut);
-    })
-
-
-    panel.addEventListener("zoom", (e) => {
-      const detail = (e as any).detail;
-      setCanZoomIn(detail.canZoomIn);
-      setCanZoomOut(detail.canZoomOut);
-    });
-    allEvents.forEach(type => {
-      panel.addEventListener(type, (e) => { action(type)(e) });
-    })
-
-  }, [document.querySelector(selector) !== undefined]);
-
-
-  return <>
-    <button disabled={!canZoomIn} onClick={() => (document?.querySelector(selector) as any).zoomIn()}>Zoom In</button> 
-    <button disabled={!canZoomOut} onClick={() => (document?.querySelector(selector) as any).zoomOut()}>Zoom Out</button>
-    {/* @ts-ignore */}
-    <canvas-panel manifest-id='https://media.getty.edu/iiif/manifest/6a744965-6345-41cf-8885-69dd07e25008' canvas-id='https://media.getty.edu/iiif/manifest/78697a2b-31b0-47d9-b1b6-32d7fd67d12c' />
-  </>
+  return ImageViewer(
+    {
+      manifestUrl: 'https://media.getty.edu/iiif/manifest/6a744965-6345-41cf-8885-69dd07e25008',
+      canvasId : 'https://media.getty.edu/iiif/manifest/78697a2b-31b0-47d9-b1b6-32d7fd67d12c'
+    }
+  )
 }
 
 
-export const CanvasWithSkipSizes = () => {
 
+function ImageViewer(props) {
 
-  const manifestUrl = saintGines;
-  const [canvases, setCanvses] = useState(['https://media.getty.edu/iiif/manifest/canvas/bb72d5f1-e230-4797-a7dc-262bf948b256']);
+  const manifestUrl = props.manifestUrl;
+  const [canvases, setCanvses] = useState([]);
   const [cvindex, setCvindex] = useState(0);
   const [zoomInfo, setZoomInfo] = useState({});
   const [canZoomIn, setCanZoomIn] = useState(false);
@@ -117,6 +65,7 @@ export const CanvasWithSkipSizes = () => {
 
 
   let panel;
+  let count = 0;
   useEffect(() => {
     panel = document.querySelector(selector);
     let manifest = undefined;
@@ -132,6 +81,10 @@ export const CanvasWithSkipSizes = () => {
       setZoomInfo(detail);
       setCanZoomIn(detail.canZoomIn);
       setCanZoomOut(detail.canZoomOut);
+      if (props.onWorldReady &&  count == 0) {
+        props.onWorldReady(panel);
+        count++;
+      }
     })
 
 
@@ -149,6 +102,7 @@ export const CanvasWithSkipSizes = () => {
   {/* @ts-ignore */ }
 
   return <>
+      {props.extra ? props.extra(panel) : ""}
     <button onClick={()=>setCvindex(c => (cvindex - 1) % canvases.length)}>Prev Canvas</button>
     <button onClick={() => setCvindex(c => (cvindex + 1) % canvases.length)}>Next Canvas</button>
     <button disabled={!canZoomIn} onClick={() => (document?.querySelector(selector) as any).zoomIn()}>Zoom In</button> 
@@ -156,8 +110,16 @@ export const CanvasWithSkipSizes = () => {
 
     { canvases[Math.abs(cvindex)] }
     {/* @ts-ignore */}
-    <canvas-panel manifest-id={manifestUrl} skip-sizes='true' canvas-id={canvases[Math.abs(cvindex)]  } />
+    <canvas-panel manifest-id={manifestUrl} skip-sizes={props.skipSizes? props.skipSizes : false} canvas-id={props.canvasId ? props.canvasId : canvases[Math.abs(cvindex)]  } />
   </>
+
+}
+
+
+export const CanvasWithSkipSizes = () => {
+
+
+  return ImageViewer({manifestUrl: saintGines, skipSizes: true})
 
 }
 
@@ -167,93 +129,32 @@ const contentStateWideViewport = "JTdCJTIyaWQlMjIlM0ElMjJodHRwcyUzQSUyRiUyRmlpaW
 export const CanvasWithMultipleContentStates = () => {
 
 
-  const manifestUrl = welcome;
+  const props = {
+    manifestUrl: welcome,
+    skipSizes: true,
+    extra: () => <>
+      <button onClick={()=> (document?.querySelector(selector) as any).setContentStateFromText(contentStateNarrowViewport)} >Narrow</button>
+      <button onClick={()=> (document?.querySelector(selector) as any).setContentStateFromText(contentStateWideViewport)} >Wide</button>
+    </>
+  }
 
-  let panel;
-  let count = 0;
-  useEffect(() => {
-    panel = document.querySelector(selector);
-    panel.addEventListener("world-ready", (e) => {
-    })
-
-
-    allEvents.forEach(type => {
-      panel.addEventListener(type, (e) => { action(type)(e) });
-    })
-
-  }, [document.querySelector(selector) !== undefined]);
-  {/* @ts-ignore */ }
-
-  return <>
-    <button onClick={()=> panel.setContentStateFromText(contentStateNarrowViewport)} >Narrow</button>
-    <button onClick={()=> panel.setContentStateFromText(contentStateWideViewport)} >Wide</button>
-    {/* @ts-ignore */}
-    <canvas-panel manifest-id={welcome} canvas-id={ canvases[0]} skip-sizes='true' />
-  </>
-
+  return ImageViewer(props)
 }
 
 
 
 export const CanvasWithContentState = () => {
 
+  const props = {
+    manifestUrl: saintGines,
+    skipSizes: true,
+    onWorldReady: (panel) =>
+            panel.setContentStateFromText("JTdCJTIyaWQlMjIlM0ElMjJodHRwcyUzQSUyRiUyRm1lZGlhLmdldHR5LmVkdSUyRmlpaWYlMkZtYW5pZmVzdCUyRmNhbnZhcyUyRmJiNzJkNWYxLWUyMzAtNDc5Ny1hN2RjLTI2MmJmOTQ4YjI1Ni5qc29uJTIzeHl3aCUzRDEwMzMuNDE0NTUwNzgxMjUlMkM0ODMzLjkxNzQ4MDQ2ODc1JTJDMjY1My4zMzEyOTg4MjgxMjUlMkMxMTY3LjEwMTU2MjUlMjIlMkMlMjJ0eXBlJTIyJTNBJTIyQ2FudmFzJTIyJTJDJTIycGFydE9mJTIyJTNBJTVCJTdCJTIyaWQlMjIlM0ElMjJodHRwcyUzQSUyRiUyRm1lZGlhLmdldHR5LmVkdSUyRmlpaWYlMkZtYW5pZmVzdCUyRjFlMGVkNDdlLTVhNWItNGZmMC1hZWEwLTQ1YWJlZTc5M2ExYyUyMiUyQyUyMnR5cGUlMjIlM0ElMjJNYW5pZmVzdCUyMiU3RCU1RCU3RA")
 
-  const manifestUrl = saintGines;
-  const [canvases, setCanvses] = useState(['https://media.getty.edu/iiif/manifest/canvas/bb72d5f1-e230-4797-a7dc-262bf948b256']);
-  const [cvindex, setCvindex] = useState(0);
-  const [zoomInfo, setZoomInfo] = useState({});
-  const [canZoomIn, setCanZoomIn] = useState(false);
-  const [canZoomOut, setCanZoomOut] = useState(false);
-
-
-  let panel;
-  let count = 0;
-  useEffect(() => {
-    panel = document.querySelector(selector);
-    let manifest = undefined;
-    panel.vault.loadManifest(manifestUrl).then((_manifest) => {
-      manifest = _manifest;
-      setCanvses((panel as any).vault.get(manifestUrl).items.map(item => item.id));
-    })
-
-    
-    panel.addEventListener("world-ready", (e) => {
-      // set the initial state based on the image that's loaded into the canvas
-      const detail = (e as any).detail;
-      setZoomInfo(detail);
-      setCanZoomIn(detail.canZoomIn);
-      setCanZoomOut(detail.canZoomOut);
-      if (count == 0) {
-        panel.setContentStateFromText("JTdCJTIyaWQlMjIlM0ElMjJodHRwcyUzQSUyRiUyRm1lZGlhLmdldHR5LmVkdSUyRmlpaWYlMkZtYW5pZmVzdCUyRmNhbnZhcyUyRmJiNzJkNWYxLWUyMzAtNDc5Ny1hN2RjLTI2MmJmOTQ4YjI1Ni5qc29uJTIzeHl3aCUzRDEwMzMuNDE0NTUwNzgxMjUlMkM0ODMzLjkxNzQ4MDQ2ODc1JTJDMjY1My4zMzEyOTg4MjgxMjUlMkMxMTY3LjEwMTU2MjUlMjIlMkMlMjJ0eXBlJTIyJTNBJTIyQ2FudmFzJTIyJTJDJTIycGFydE9mJTIyJTNBJTVCJTdCJTIyaWQlMjIlM0ElMjJodHRwcyUzQSUyRiUyRm1lZGlhLmdldHR5LmVkdSUyRmlpaWYlMkZtYW5pZmVzdCUyRjFlMGVkNDdlLTVhNWItNGZmMC1hZWEwLTQ1YWJlZTc5M2ExYyUyMiUyQyUyMnR5cGUlMjIlM0ElMjJNYW5pZmVzdCUyMiU3RCU1RCU3RA")
-      }
-      count++;
-    })
+  }
 
 
-    panel.addEventListener("zoom", (e) => {
-      const detail = (e as any).detail;
-      setZoomInfo(detail);
-      setCanZoomIn(detail.canZoomIn);
-      setCanZoomOut(detail.canZoomOut);
-    });
-    allEvents.forEach(type => {
-      panel.addEventListener(type, (e) => { action(type)(e) });
-    })
-
-  }, [document.querySelector(selector) !== undefined]);
-  {/* @ts-ignore */ }
-
-  return <>
-    <button onClick={()=>setCvindex(c => (cvindex - 1) % canvases.length)}>Prev Canvas</button>
-    <button onClick={() => setCvindex(c => (cvindex + 1) % canvases.length)}>Next Canvas</button>
-    <button disabled={!canZoomIn} onClick={() => (document?.querySelector(selector) as any).zoomIn()}>Zoom In</button> 
-    <button disabled={!canZoomOut} onClick={() => (document?.querySelector(selector) as any).zoomOut()}>Zoom Out</button>
-
-    { canvases[Math.abs(cvindex)] }
-    {/* @ts-ignore */}
-    <canvas-panel manifest-id={manifestUrl} skip-sizes='true' canvas-id={canvases[Math.abs(cvindex)]  } />
-  </>
-
+  return ImageViewer(props)
 }
 
 
